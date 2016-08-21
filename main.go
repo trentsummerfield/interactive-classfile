b3308738a700a875430c855497ebd99a22375d4f
Trent Summerfield <trent.summerfield@gmail.com>
2016-08-21 18:08:30 +1000

    Spit out the contents of HelloWorld.java

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/hex"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -8,7 +10,36 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Page struct {
+	ClassFileBytes []string
+	JavaSource     string
+	Err            error
+}
+
 func main() {
+	javaFile, javaErr := ioutil.ReadFile("static/HelloWorld.java")
+	classFile, classErr := ioutil.ReadFile("static/HelloWorld.class")
+
+	var err error
+	if javaErr != nil {
+		err = javaErr
+	} else {
+		err = classErr
+	}
+
+	hexString := hex.EncodeToString(classFile)
+	var classString []string
+	len := len(hexString)
+	for i := 0; i < len; i += 2 {
+		classString = append(classString, hexString[i:i+2])
+	}
+
+	page := Page{
+		ClassFileBytes: classString,
+		JavaSource:     string(javaFile),
+		Err:            err,
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("$PORT must be set")
@@ -17,7 +48,7 @@ func main() {
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*.tmpl.html")
 	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+		c.HTML(http.StatusOK, "index.tmpl.html", page)
 	})
 	r.Run(":" + port)
 }
